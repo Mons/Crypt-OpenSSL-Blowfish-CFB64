379bb54b5930c3e18b654be57efe38c7d3c57d78
Mons Anderson <mons@cpan.org>
2012-03-16 17:36:37 +0400

    initial

---
+++ makeall.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+MODULE=`perl -ne 'print($1),exit if m{VERSION_FROM.+?([\w/.]+)}i' Makefile.PL`;
+
+perl=perl
+$perl -v
+
+rm -rf MANIFEST.bak Makefile.old && \
+pod2text $MODULE > README && \
+#$perl -i -lpne 's{^\s+$}{};s{^    ((?: {8})+)}{" "x(4+length($1)/2)}se;' README && \
+$perl Makefile.PL && \
+make manifest && \
+make && \
+TEST_AUTHOR=1 make test && \
+#perl tmp/test.pl bench && \
+#perl -MDevel::Leak tmp/test.pl && \
+#perl -MTest::Valgrind tmp/test.pl && \
+#DEBUG_MEM=1 perl tmp/test.pl && \
+#TEST_AUTHOR=1 runprove 'xt/*.t' && \
+make disttest && \
+make clean && \
+$perl Makefile.PL && \
+make dist && \
+cp -f *.tar.gz dist/ && \
+make clean && \
+rm -rf MANIFEST.bak Makefile.old && \
+echo "All is OK"
